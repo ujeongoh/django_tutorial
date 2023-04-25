7d2ad42a671f0ec7688fcfe4f7a10ccfba198c06
ujeongoh <yujoh94@gmail.com>
2023-04-25 15:11:33 +0900

    modify index.html - using context

--- polls/views.py
+++ polls/views.py
@@ -7,8 +7,9 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     #output = ', '.join([q.question_text for q in latest_question_list])
     #return HttpResponse(output)
-    # context = {
-    #     'first_question': latest_question_list[0]
-    # }
+    context = {
+        # template에서 'first_question'으로 해당 값에 접근할 수 있다.
+        'first_question': latest_question_list[0]
+    }
     
-    return render(request, 'polls/index.html')
+    return render(request, 'polls/index.html', context)
