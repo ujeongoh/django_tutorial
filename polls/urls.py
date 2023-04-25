6a3fa2cdc203f5001fdc8246c3863a977bb6d516
ujeongoh <yujoh94@gmail.com>
2023-04-25 16:02:01 +0900

    add app name to polls

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# 앱 이름 설정 - html파일에서 name으로 url을 접근할 때 이제 앞에 'polls:'도 붙여줘야 함
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>', views.detail, name='detail')
