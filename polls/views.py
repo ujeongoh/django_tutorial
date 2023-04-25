9598a591732f81374c183528bc722c4f5bdc287d
ujeongoh <yujoh94@gmail.com>
2023-04-25 16:13:25 +0900

    add 404 exceptions

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import *
 
 # Create your views here.
@@ -16,9 +16,18 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = Question.objects.get(pk=question_id)
+    # 주어진 question_id를 가진 Question이 없을 경우 예외처리
+
+    # 1. try~except 로 예외처리
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist!")
+
+    # 2. get_object_or_404 메소드로 예외처리
+    question = get_object_or_404(Question, pk=question_id)
     context = {
         'question': question
     }
-    #return HttpResponse(f'입력받은 질문id입니다 : {question_id}')
+    
     return render(request, 'polls/detail.html', context)
