9989e7ce1801debdd43d5d73c49fab08c0195730
ujeongoh <yujoh94@gmail.com>
2023-04-25 15:06:55 +0900

    add polls templates

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+from datetime import datetime, timedelta
 
 # Create your models here.
 class Question(models.Model):
@@ -6,7 +8,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - timedelta(days=1)
     
     def __str__(self):
         if self.was_published_recently():
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    #return HttpResponse(output)
+    # context = {
+    #     'first_question': latest_question_list[0]
+    # }
+    
+    return render(request, 'polls/index.html')
