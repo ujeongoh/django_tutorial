caea14bf0b993152334af31b62c6287ec4517934
ujeongoh <yujoh94@gmail.com>
2023-04-27 17:19:20 +0900

    add user field to Question model

--- polls/models.py
+++ polls/models.py
@@ -7,6 +7,7 @@ from datetime import datetime, timedelta
 class Question(models.Model):
     question_text = models.CharField(max_length=200, verbose_name='질문')
     pub_date = models.DateTimeField(auto_now=True, verbose_name='생성일시')
+    owner = models.ForeignKey('auth.User', related_name='questions', on_delete=models.CASCADE, null=True)
 
     @admin.display(boolean=True, description='최근생성(하루기준)')
     def was_published_recently(self):
