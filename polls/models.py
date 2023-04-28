29132c4334ac9544b69b663469d266b2216c5fe8
ujeongoh <yujoh94@gmail.com>
2023-04-28 12:33:01 +0900

    add choiceserializer

--- polls/models.py
+++ polls/models.py
@@ -5,15 +5,18 @@ from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 
 # Create your models here.
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200, verbose_name='질문')
     pub_date = models.DateTimeField(auto_now=True, verbose_name='생성일시')
-    owner = models.ForeignKey('auth.User', related_name='questions', on_delete=models.CASCADE, null=True)
+    owner = models.ForeignKey(
+        'auth.User', related_name='questions', on_delete=models.CASCADE, null=True)
 
     @admin.display(boolean=True, description='최근생성(하루기준)')
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - timedelta(days=1)
-    
+
     def __str__(self):
         if self.was_published_recently():
             new_badge = 'NEW!!!'
@@ -21,11 +24,12 @@ class Question(models.Model):
             new_badge = ''
         return f'{new_badge} 제목: {self.question_text}, 날짜: {self.pub_date}'
 
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(
+        Question, related_name='choices', on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
         return f'[{self.question.question_text}] {self.choice_text}'
-
--- polls_api/serializers.py
+++ polls_api/serializers.py
@@ -1,24 +1,34 @@
 from rest_framework import serializers
-from polls.models import Question  # serialize 할 모델 가져오기
+from polls.models import *  # serialize 할 모델 가져오기
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ['choice_text', 'votes']
 
 
 class QuestionSerializer(serializers.ModelSerializer):
     # 새로운 질문이 생성될 때 owner 필드에 현재 요청을 보낸 사용자를 자동으로 할당
     owner = serializers.ReadOnlyField(source='owner.username')
+    choices = ChoiceSerializer(many=True, read_only=True)
 
     class Meta:
         # Meta 클래스 내부의 model 속성을 통해 Serialize할 대상 모델을 지정할 수 있다.
         model = Question
         # Meta 클래스 내부의 fields 속성을 통해 Serialize할 대상 필드들을 리스트 형태로 나열하여 지정할 수 있다.
-        fields = ['id', 'question_text', 'pub_date', 'owner']
+        fields = ['id', 'question_text', 'pub_date', 'owner', 'choices']
 
 
 class UserSerializer(serializers.ModelSerializer):
     # User 의 id를 통해서 불러들일 수 있는 question들을 명시한다.
-    questions = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Question.objects.all())
+    # questions = serializers.StringRelatedField(many=True, read_only=True)
+    # questions = serializers.PrimaryKeyRelatedField(many=True, queryset=Question.objects.all())
+    # questions = serializers.SlugRelatedField(many=True, read_only=True, slug_field='pub_date')
+    questions = serializers.HyperlinkedRelatedField(
+        many=True, read_only=True, view_name='question-detail')
 
     class Meta:
         model = User
